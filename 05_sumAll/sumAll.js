c5fd3f1adddd759366c78b21d63e678794c0c7bc
redpho <medasdan@gmail.com>
2022-09-10 20:30:47 +0300

    Do 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  if (typeof x != "number" || typeof y != "number" || x < 0 || y < 0) {
+    return "ERROR"
+  }
+  if (x < y) {
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+  }
+  else if (y < x) {
+    for (let i = y; i <= x; i++) {
+      sum += i;
+    }
+  }
+  return sum
 };
 
 // Do not edit below this line
